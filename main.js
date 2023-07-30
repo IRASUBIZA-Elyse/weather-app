5a55b3407b947d5f11a16afdaef3cb706f7ced2b
IRASUBIZA Elyse <12356789+odin@users.norely.github.com>
2023-07-30 22:17:43 +0200

    found the weather info of the hard coded city

--- main.js
+++ main.js
@@ -4,22 +4,36 @@ let temperature = document.querySelector('.temperature-degree');
 let description = document.querySelector('.description');
 let humidity = document.querySelector('.humidity');
 let windSpeed = document.querySelector('.wind');
-let cityName = search.value
+
 
 const apiKey = "e18c323ec08b4378ba2115015232707"
+// let cityn= "kampala"
 
-function searchWeather(city)
+function searchWeather(city){
+    console.log(city)
 fetch("https://api.weatherapi.com/v1/current.json?key="+apiKey+"&q="+city+"")
 .then(respo => respo.json())
 .then(data => displayWeather(data))
-
+.catch(err => error(err))
+}
 function displayWeather(data){
-    cityContent.innerHTML =`Weather in${city}`
-    temperature.innerHTML = "C";
-    description.innerHTML = 
-    humidity.innerHTML = "Humidity:"+ + "%"
-    windSpeed.innerHTML = "Wind Speed" + + "km/hr"
+    console.log(data)
+    cityContent.textContent =`Weather in ${data.location.name}`
+    temperature.textContent = `${data.current.temp_c} °C`;
+    description.textContent = `${data.current.condition.text}`
+    humidity.textContent = `Humidity: ${data.current.humidity} %`
+    windSpeed.textContent = `Wind Speed: ${data.current.wind_kph} km/hr`
 }
 
-document.querySelector(".search button").addEventListener("click",searchWeather(cityName))
+function error(err){
+    console.log(err)
+    if(err.code === 400){
+        return alert("the city doesn't exist")
+    }else {
+        console.log(err)
+    }
+}
+document.querySelector(".btn").addEventListener("click", ()=>{
+    const inValue = search.value
+    searchWeather(inValue)})
 
