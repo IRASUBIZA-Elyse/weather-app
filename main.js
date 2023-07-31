a870957b75874753802b0c8f3dcf71509d1f12e9
IRASUBIZA Elyse <12356789+odin@users.norely.github.com>
2023-07-31 22:04:06 +0200

    added the event listener to keyboard

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let temperature = document.querySelector('.temperature-degree');
 let description = document.querySelector('.description');
 let humidity = document.querySelector('.humidity');
 let windSpeed = document.querySelector('.wind');
-
+let icon = document.querySelector('.icon')
 
 const apiKey = "e18c323ec08b4378ba2115015232707"
 // let cityn= "kampala"
@@ -20,6 +20,7 @@ function displayWeather(data){
     console.log(data)
     cityContent.textContent =`Weather in ${data.location.name}`
     temperature.textContent = `${data.current.temp_c} °C`;
+    icon.src = `${current.condition.icon}`
     description.textContent = `${data.current.condition.text}`
     humidity.textContent = `Humidity: ${data.current.humidity} %`
     windSpeed.textContent = `Wind Speed: ${data.current.wind_kph} km/hr`
@@ -34,6 +35,13 @@ function error(err){
     }
 }
 document.querySelector(".btn").addEventListener("click", ()=>{
-    const inValue = search.value
+    let inValue = search.value
     searchWeather(inValue)})
-
+search.addEventListener("keyup", (event) =>{
+    
+    if(event.key == "Enter"){
+        let newValue = search.value
+        // console.log(event);
+        searchWeather(newValue);
+    }
+})
